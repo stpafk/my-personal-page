14bda1353de1a480e6ad16560f41dcbf2377d928
stpafk <stephan.weigert@hotmail.com>
2023-08-28 20:22:04 -0300

    create sidebar

--- personal-page/src/components/Sidebar.jsx
+++ personal-page/src/components/Sidebar.jsx
@@ -1,33 +1,34 @@
 
 export default function Sidebar() {
 
+
     return(
         <nav>
             <div className="header__img">
-                <img src="" alt="some icon" />
+                <i className="bi bi-menu-button-wide"></i>
             </div>
             <ul>
                 <li className="sidebar__li">
                     <a href="#">
-                        <span className="icon"><i class="bi bi-house"></i></span>
+                        <span className="icon"><i className="bi bi-house"></i></span>
                         <span className="txt-click">Home</span>
                     </a>
                 </li>
                 <li className="sidebar__li">
                     <a href="#">
-                        <span className="icon"><i class="bi bi-braces"></i></span>
+                        <span className="icon"><i className="bi bi-braces"></i></span>
                         <span className="txt-click">Technology</span>
                     </a>
                 </li>
                 <li className="sidebar__li">
                     <a href="#">
-                        <span className="icon"><i class="bi bi-lightbulb"></i></span>
+                        <span className="icon"><i className="bi bi-lightbulb"></i></span>
                         <span className="txt-click">Projects</span>
                     </a>
                 </li>
                 <li className="sidebar__li">
                     <a href="#">
-                        <span className="icon"><i class="bi bi-file-person"></i></span>
+                        <span className="icon"><i className="bi bi-file-person"></i></span>
                         <span className="txt-click">Contact</span>
                     </a>
                 </li>
