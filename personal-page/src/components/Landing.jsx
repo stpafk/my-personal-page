691cd075aef35b10cd6ea6ce9501d54d5b6e0259
stpafk <stephan.weigert@hotmail.com>
2023-08-29 09:53:14 -0300

    add article

--- personal-page/src/components/Landing.jsx
+++ personal-page/src/components/Landing.jsx
@@ -10,14 +10,25 @@ export default function Landing() {
             </div>
             <div className="bar"></div>
             <article className="presentation">
-                <p className="presentation__text">
-                    Hi! My name is Stephan and I am an aspiring Fullstack Web Developer.
-                    My main stack currently is JavaScript (React & Node), but I'm always
-                    looking to learn new stuff.
-                    My focus in web developing is to deliver an easy-to-use, accessible and
-                    maintainable application. Hover by to see some of my knowledges and
-                    projects!
-                </p>
+                <div className="presentation__info">
+                    <h1>Introduction</h1>
+                    <p className="presentation__text">
+                        Hi! My name is Stephan and I am an aspiring Fullstack Web Developer.
+                        My main stack currently is JavaScript (React & Node), but I'm always
+                        looking to learn new stuff.
+                    </p> 
+
+                    <h1>Goals</h1>
+                    <p className="presentation__goals">
+                        As an aspiring Junior Developer, my goals are to not only to build easy-to-use, accessible and
+                        maintanable applications, but also, to create a communicative and assertive work environment.
+                    </p>
+
+                    <label htmlFor="checkout__tech">Check my technologies!</label>
+                    <input type="button" value="Check Technologies" />
+
+                    
+                </div>
                 <img src={img} alt="Stephan's Img" />
             </article>
         </section>
