6201b135783ab00af4811c046d0b94b93ca3b344
stpafk <stephan.weigert@hotmail.com>
2023-08-30 10:56:39 -0300

    feat: add icons

--- personal-page/src/components/Technology.jsx
+++ personal-page/src/components/Technology.jsx
@@ -1,3 +1,5 @@
+import techs from "../helpers/tech"
+
 
 export default function Technology() {
 
@@ -9,7 +11,13 @@ export default function Technology() {
                         <h3>Here are the technologies which I have some experience on.</h3>
                     </div>
                     <div className="tech__box">
-                        
+                        <ul className="tech__ul">
+                            {techs.map((item) => (
+                                <li className="grid_tech">
+                                    <i className={item.i}></i>
+                                </li>
+                            ))}
+                        </ul>
                     </div>
                     
             </section>
---
+++ personal-page/src/helpers/tech.jsx
@@ -0,0 +1,56 @@
+
+
+const techs = [
+
+    {   
+        name: "JavaScript",
+        i: "devicon-javascript-plain colored",
+        exp: "5",
+    },
+
+    {
+        name: "TypeScript",
+        i: "devicon-typescript-plain colored",
+        exp: "3",
+    },
+
+    {
+        name: "React",
+        i: "devicon-react-original colored",
+        exp: "5",
+    },
+
+    {
+        name: "Webpack",
+        i: "devicon-webpack-plain colored",
+        exp: "3"
+    },
+
+    {
+        name: "CSS3",
+        i: "devicon-css3-plain colored",
+        exp: "5"
+    },
+
+    {
+        name: "HTML5",
+        i: "devicon-html5-plain colored",
+        exp: "5"
+    },
+
+    {
+        name: "Git",
+        i: "devicon-git-plain colored",
+        exp: "4",
+    },
+
+    {
+        name: "Python",
+        i: "devicon-python-plain-wordmark colored",
+        exp: "5",
+    },
+
+
+]
+
+export default techs;
