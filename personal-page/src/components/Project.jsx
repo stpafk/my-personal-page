f280addc48ca0fd7417f567a63296b6aa5cb961f
stpafk <stephan.weigert@hotmail.com>
2023-08-31 21:50:40 -0300

    add hover function

--- personal-page/src/components/Project.jsx
+++ personal-page/src/components/Project.jsx
@@ -1,12 +1,22 @@
 
 export default function Project() {
 
+    function upZIndex(e) { e.target.style = "z-index: 3"}
+    function downZIndex(e) {e.target.style = ""}
+
+
     return(
         <section className="project__section">
             <div className="project__wrapper">
-                <div className="wrapper first" id="0"></div>
-                <div className="wrapper sec" id="1"></div>
-                <div className="wrapper third" id="2"></div>
+                <div className="wrapper first" 
+                onMouseOver={(e) => upZIndex(e)}
+                onMouseLeave={(e) => downZIndex(e)} id="3"></div>
+                <div className="wrapper sec" id="1"
+                onMouseOver={(e) => upZIndex(e)}
+                onMouseLeave={(e) => downZIndex(e)}></div>
+                <div className="wrapper third" id="0"
+                onMouseOver={(e) => upZIndex(e)}
+                onMouseLeave={(e) => downZIndex(e)}></div>
             </div>
         </section>
     )
