00b555d4434346080253d9716ac976d777b297b8
stpafk <stephan.weigert@hotmail.com>
2023-09-27 09:15:13 -0300

    add svg

--- personal-page/src/components/Contact.jsx
+++ personal-page/src/components/Contact.jsx
@@ -1,10 +1,25 @@
+import Email from '../assets/email.svg';
 
 export default function Contact() {
     return(
         <section id="contact" className="contact__section">
             <div className="contact__header">
                 <h1>Contact</h1>
-                <p>Here are myy contact links, including my email, linkedin and github.</p>
+                <p>Here are my contact links, including my email, linkedin and github.</p>
+            </div>
+
+            <div className="contact__links">
+                <ul>
+                    <li className="grid__contact">
+                        <img id="linkedin" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" alt='LinkedIn' />
+                    </li>
+                    <li className="grid__contact">
+                        <img id="github" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt='Github' />
+                    </li>
+                    <li className="grid__contact">
+                        <img id="email" src={Email} alt="Email" />
+                    </li>
+                </ul>
             </div>
         </section>
     )
